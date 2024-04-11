46825f19bb92c99182b21a4fa80b2caae9e3c575
Calvin Owens <calvin@wbinvd.org>
2024-04-11 13:20:19 -0700

    Update kconfigs, simplify build script

--- scripts/make-image.sh
+++ scripts/make-image.sh
@@ -1,27 +1,27 @@
 #!/bin/bash -e
 
 STAGETARBALL=""
-HEADLESSTARBALL=""
-HEADLESSEXEC="/opt/run"
 IMGSIZE="10G"
 SERCON="ttyS"
-NETCON="enp0s2"
+NETCON="enp1s0"
+IMGXCOMP="zstd:9"
+IMGCOMP="zstd:1"
 
 HELPTEXT="Usage: ./make-image.sh [opts] <rootfs_tar_path> <disk_path>
-	-H [tar_path]	Build a headless /opt/run image, with [tar_path] as /opt
-	-E [vm_path]	Path to execute headlessly (default ${HEADLESSEXEC})
-	-s [size]	Size of sprase file for new disks (default ${IMGSIZE})
+	-s [size]	Size of sparse file for new disks (default ${IMGSIZE})
 	-c [serial]	Prefix for serial device (default ${SERCON})
 	-n [netcon]	Netdevice, only needed for openrc (default ${NETCON})
+	-x [level]	Compression for extraction (default ${IMGXCOMP})
+	-z [level]	Compression for VM fstab (default ${IMGCOMP})
 "
 
-while getopts H:E:s:c:n:h i; do
+while getopts s:c:n:x:l:h i; do
 	case $i in
-		H) HEADLESSTARBALL="${OPTARG}" ;;
-		E) HEADLESSEXEC="${OPTARG}" ;;
 		s) IMGSIZE="${OPTARG}" ;;
 		c) SERCON="${OPTARG}" ;;
 		n) NETCON="${OPTARG}" ;;
+		x) IMGXCOMP="${OPTARG}" ;;
+		z) IMGCOMP="${OPTARG}" ;;
 		h) echo "${HELPTEXT}"; exit ;;
 	esac
 done
@@ -66,24 +66,28 @@ COSNAME="${COSNAME%%".img"}"
 
 echo "Unpacking ${STAGETARBALL} into ${IMG}"
 sudo mkfs.btrfs --csum blake2 -m single -d single -q ${LOOPDEV}
-sudo mount -o noatime,discard ${LOOPDEV} mnt
+sudo mount -o compress=${IMGXCOMP},noatime,discard ${LOOPDEV} mnt
 sudo tar xfs ${STAGETARBALL} -C mnt
+
+sudo bash -c 'cat > mnt/etc/fstab' <<-END
+/dev/vda / btrfs compress=${IMGCOMP},noatime,discard 0 1
+END
 
 echo "Unpacking portage snapshot into ${IMG}"
 if [ ! -e tarballs/portage.tar.xz ]; then
 	wget https://distfiles.gentoo.org/snapshots/portage-latest.tar.xz \
 		-O tarballs/portage.tar.xz
 fi
-sudo chattr +C mnt/var/db/repos
+sudo mkdir -p mnt/etc/portage/repos.conf
+sudo cp mnt/usr/share/portage/config/repos.conf \
+	mnt/etc/portage/repos.conf/gentoo.conf
 sudo tar xfs tarballs/portage.tar.xz -C mnt/var/db/repos
 sudo mv mnt/var/db/repos/portage mnt/var/db/repos/gentoo
 sudo bash -c 'cat >> mnt/etc/portage/make.conf' <<-END
-
-FEATURES="${FEATURES} binpkg-request-signature"
+USE="\${USE} verify-sig"
+FEATURES="\${FEATURES} binpkg-request-signature"
 EMERGE_DEFAULT_OPTS="-g"
 END
-
-sudo bash -c 'echo "/dev/vda / btrfs noatime,discard 0 1" > mnt/etc/fstab'
 
 echo "Hostname: '${COSNAME}'"
 if [ -e mnt/etc/systemd/ ]; then
@@ -100,6 +104,19 @@ if [ -e mnt/etc/systemd/ ]; then
 	DHCP=ipv4
 	END
 	sudo chattr +C mnt/var/log/journal/
+	sudo mkdir -p mnt/etc/systemd/system/getty@hvc0.service.d/
+	sudo bash -c "cat > mnt/etc/systemd/system/getty@hvc0.service.d/override.conf" <<-END
+	[Service]
+	Type=simple
+	ExecStart=
+	ExecStart=-/sbin/agetty --autologin root --noclear %I 115200 linux
+	TimeoutSec=1800
+	RestartSec=5
+	END
+	sudo rm mnt/etc/systemd/system/getty.target.wants/*
+	sudo ln -s /dev/null mnt/etc/systemd/system/serial-getty@${SERCON}0.service
+	sudo ln -s /usr/lib/systemd/system/getty@.service \
+		mnt/etc/systemd/system/getty.target.wants/getty@hvc0.service
 else
 	sudo bash -c "cat > mnt/etc/conf.d/hostname <<< \"hostname=\\\"${COSNAME}\\\"\""
 	sudo bash -c 'cat > mnt/etc/conf.d/net' <<-END
@@ -107,52 +124,12 @@ else
 	END
 	sudo ln -s net.lo mnt/etc/init.d/net.${NETCON}
 	sudo ln -s /etc/init.d/net.${NETCON} mnt/etc/runlevels/default/net.${NETCON}
-	sudo bash -c 'cat /etc/timezone > mnt/etc/timezone'
-fi
-
-if [ -z "${HEADLESSTARBALL}" ]; then
-	if [ -e mnt/etc/systemd/ ]; then
-		sudo mkdir -p mnt/etc/systemd/system/getty@hvc0.service.d/
-		sudo bash -c "cat > mnt/etc/systemd/system/getty@hvc0.service.d/override.conf" <<-END
-		[Service]
-		Type=simple
-		ExecStart=
-		ExecStart=-/sbin/agetty --autologin root --noclear %I 115200 linux
-		TimeoutSec=1800
-		RestartSec=5
-		END
-		sudo rm mnt/etc/systemd/system/getty.target.wants/*
-		sudo ln -s /dev/null mnt/etc/systemd/system/serial-getty@${SERCON}0.service
-		sudo ln -s /usr/lib/systemd/system/getty@.service \
-			mnt/etc/systemd/system/getty.target.wants/getty@hvc0.service
-	else
-		sudo sed -ri 's/^s0/#s0/g' mnt/etc/inittab
-		sudo sed -ri 's/^s1/#s1/g' mnt/etc/inittab
-		sudo bash -c "cat >> mnt/etc/inittab" <<-END
-		s0:12345:respawn:/sbin/agetty hvc0 linux -a root
-		END
-	fi
-else
-	if [ -e mnt/etc/systemd/ ]; then
-		sudo mkdir -p mnt/etc/systemd/system/getty@${SERCON}0.service.d/
-		sudo bash -c "cat > mnt/etc/systemd/system/getty@${SERCON}0.service.d/override.conf" <<-END
-		[Service]
-		Type=simple
-		ExecStart=
-		ExecStart=-${HEADLESSEXEC}
-		TimeoutSec=1800
-		RestartSec=5
-		END
-		sudo rm mnt/etc/systemd/system/getty.target.wants/*
-		sudo ln -s /usr/lib/systemd/system/getty@.service \
-			mnt/etc/systemd/system/getty.target.wants/getty@${SERCON}0.service
-	else
-		sudo sed -ri 's/^s0/#s0/g' mnt/etc/inittab
-		sudo sed -ri 's/^s1/#s1/g' mnt/etc/inittab
-		sudo bash -c "cat >> mnt/etc/inittab" <<-END
-		s0:12345:respawn:/opt/run
-		END
-	fi
+	sudo bash -c 'cat /etc/timezone > mnt/etc/timezone' || echo "Can't copy TZ?"
+	sudo sed -ri 's/^s0/#s0/g' mnt/etc/inittab
+	sudo sed -ri 's/^s1/#s1/g' mnt/etc/inittab
+	sudo bash -c "cat >> mnt/etc/inittab" <<-END
+	s0:12345:respawn:/sbin/agetty hvc0 linux -a root
+	END
 fi
 
 echo "done!"
