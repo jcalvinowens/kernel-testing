1f05318e5ef2968223a03e9ef5853feeebf8329d
Calvin Owens <calvin@wbinvd.org>
2026-05-07 11:14:08 -0700

    Clean up GCC build script, upgrade GCC/LLVM

--- scripts/build-gcc.sh
+++ scripts/build-gcc.sh
@@ -14,11 +14,10 @@
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 # FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-gittag_gcc13="releases/gcc-13.4.0" #"origin/releases/gcc-13"
-gittag_gcc14="releases/gcc-14.3.0" #"origin/releases/gcc-14"
+gittag_gcc16="releases/gcc-16.1.0" #"origin/releases/gcc-16"
 gittag_gcc15="releases/gcc-15.2.0" #"origin/releases/gcc-15"
 gittag_binutils="binutils-2_46" #"origin/binutils-2_46-branch"
-gittag_kheaders="v6.19"
+gittag_kheaders="v7.0" #"master"
 
 giturl_gcc="git://gcc.gnu.org/git/gcc.git"
 giturl_binutils="git://sourceware.org/git/binutils-gdb.git"
@@ -28,11 +27,9 @@ gitpath_gcc="${HOME}/git/gcc"
 gitpath_binutils="${HOME}/git/binutils"
 gitpath_kheaders="${HOME}/git/linux"
 
-tgt_gnu13="${HOME}/gnu-13"
-tgt_gnu14="${HOME}/gnu-14"
-tgt_gnu15="${HOME}/gnu-15"
-
 vendor="house"
+tgt_gnu15="${HOME}/gnu-15"
+tgt_gnu16="${HOME}/gnu-16"
 build_cflags="-O2 -march=native -mtune=native -pipe"
 nr_jobs=$[$(grep -Fc processor /proc/cpuinfo) + 1]
 
@@ -157,16 +154,17 @@ build_native() {
 	local target=${1}
 	local gittag=${2}
 
-	if [ ! -d ${target} ]; then
-		mkdir -p ${target}
-		tmpdir=$(mktemp -d)
-		build_init_binutils ${tmpdir}
-		build_gcc ${target} ${tmpdir} ${gittag}
-		build_binutils ${target} ${tmpdir}
-		rm -rf ${tmpdir}
-	else
+	if [ -d ${target} ]; then
 		echo "${target} already exists"
+		return
 	fi
+
+	mkdir -p ${target}
+	tmpdir=$(mktemp -d)
+	build_init_binutils ${tmpdir}
+	build_gcc ${target} ${tmpdir} ${gittag}
+	build_binutils ${target} ${tmpdir}
+	rm -rf ${tmpdir}
 }
 
 build_cross() {
@@ -175,14 +173,15 @@ build_cross() {
 	local system=${3}
 	local gittag=${4}
 
-	if [ ! -d ${target}-${arch} ]; then
-		mkdir -p ${target}-${arch}
-		build_cross_kheaders ${target}-${arch} ${arch}-${vendor}-${system}
-		build_cross_binutils ${target}-${arch} ${arch}-${vendor}-${system} ${target}
-		build_cross_gcc ${target}-${arch} ${arch}-${vendor}-${system} ${gittag} ${target}
-	else
+	if [ -d ${target}-${arch} ]; then
 		echo "${target}-${arch} already exists"
+		return
 	fi
+
+	mkdir -p ${target}-${arch}
+	build_cross_kheaders ${target}-${arch} ${arch}-${vendor}-${system}
+	build_cross_binutils ${target}-${arch} ${arch}-${vendor}-${system} ${target}
+	build_cross_gcc ${target}-${arch} ${arch}-${vendor}-${system} ${gittag} ${target}
 }
 
 build_gcc_set() {
@@ -190,15 +189,14 @@ build_gcc_set() {
 	local gittag=${2}
 
 	build_native ${target} ${gittag}
-	build_cross ${target} riscv64 linux ${gittag}
 	build_cross ${target} aarch64 linux ${gittag}
 	build_cross ${target} arm linux-gnueabihf ${gittag}
+	build_cross ${target} riscv64 linux ${gittag}
 }
 
 [ -d ${gitpath_gcc} ] || git clone ${giturl_gcc} ${gitpath_gcc}
 [ -d ${gitpath_binutils} ] || git clone ${giturl_binutils} ${gitpath_binutils}
 [ -d ${gitpath_kheaders} ] || git clone ${giturl_kern} ${gitpath_kheaders}
 
-build_gcc_set ${tgt_gnu13} ${gittag_gcc13}
-build_gcc_set ${tgt_gnu14} ${gittag_gcc14}
 build_gcc_set ${tgt_gnu15} ${gittag_gcc15}
+build_gcc_set ${tgt_gnu16} ${gittag_gcc16}
